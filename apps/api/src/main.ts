b4c39ff9604ced5b6222f47f6bd430edf4e561e3
Mauricio Knabben <91981855+MauKnabbenOfficial@users.noreply.github.com>
2025-09-08 01:26:03 -0300

    #3 - feat(conectando_nginx): - adicionado e configurado nginx

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,13 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.setGlobalPrefix('api');
 
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  //Em dev (origens diferentes), habilitar CORS para o front local
+  // app.enableCors();
+  // app.enableCors({
+  //   origin: 'http://localhost:4200',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
 
   await app.listen(process.env.PORT ?? 3000);
 }
